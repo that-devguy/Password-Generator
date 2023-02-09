caa9c7f804e0303626bafab7eaec2acf59b3953f
that devguy <zkmutch@gmail.com>
2023-02-08 23:03:45 -0500

    added variables for char sets used in password and added some psuedo code

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+
 function generatePassword() {
   console.log("Hey, You clicked the button!");
+// Determine the character sets: lowercase, uppercase, number, and special characters
+  var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numeric = "0123456789";
+  var special = "!@#$%^&*()_+-=[]{}|;':<>,.?";
 
+  // 1. Prompt the user for password criteria
+  //    a. Password length 8 < 128
+  // 2. Validate the input
+  // 3. Generate the password base on criteria
+
+
+  // 4. Display on page
   return "Generated password will go here!";
 }
 
