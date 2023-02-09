920f40889ae45ff56607283ea9fdc039dc1a5523
that devguy <zkmutch@gmail.com>
2023-02-08 22:22:54 -0500

    updated style href. added consolelog & return in genPw function to help determin if the button is working

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  console.log("Hey, You clicked the button!");
+
+  return "Generated password will go here!";
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
